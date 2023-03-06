5881a007e21abaf9afd01cf974430be6b6659743
subimm <subimm630@gmail.com>
2023-03-06 22:36:37 +0900

    스프링 DB 접근 기술 - AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -25,11 +25,10 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        //같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member);    // 중복 회원 검증
 
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member);    // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -43,7 +42,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-       return memberRepository.findAll();
+            return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
